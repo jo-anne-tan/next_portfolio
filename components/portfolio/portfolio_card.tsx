23b73266d1db09746fb552157f46c3ae90e78f2d
Jo-Anne Tan <tanjoanne128@gmail.com>
2022-06-21 09:07:25 +0800

    update portfolio projects

--- components/portfolio/portfolio_card.tsx
+++ components/portfolio/portfolio_card.tsx
@@ -1,14 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
+import { PortfolioData } from "../../data/portfolio_data";
 
-type Props = {
-  image: string;
-  title: string;
-  subtitle: string;
-  github_link: string;
-  website_link: string;
-  description: string[];
-};
+type Props = PortfolioData;
 const PortfolioCard: React.FC<Props> = ({
   image,
   title,
@@ -24,20 +18,22 @@ const PortfolioCard: React.FC<Props> = ({
         src={image}
         alt="screenshot of coffee connoisseur web app"
         objectFit="cover"
-        width={600}
-        height={400}
+        width={500}
+        height={800}
       />
       <div className="md:w-1/2 mt-5 md:mt-0">
         <h1 className="text-3xl">{title}</h1>
         <p className="mt-2">{subtitle}</p>
         <div className="flex gap-x-5 mt-2">
-          <Link href={github_link}>
-            <a className="text-cyan-400 hover:text-rose-400">
-              View Github Repository
-            </a>
-          </Link>
-          <Link href={website_link}>
-            <a className="text-cyan-400 hover:text-rose-400">
+          {github_link && (
+            <Link href={github_link} passHref>
+              <a target="_blank" className="text-cyan-400 hover:text-rose-400">
+                View Github Repository
+              </a>
+            </Link>
+          )}
+          <Link href={website_link} passHref>
+            <a target="_blank" className="text-cyan-400 hover:text-rose-400">
               View Live Website
             </a>
           </Link>
--- data/portfolio_data.ts
+++ data/portfolio_data.ts
@@ -1,8 +1,8 @@
-type PortfolioData = {
+export type PortfolioData = {
   image: string;
   title: string;
   subtitle: string;
-  github_link: string;
+  github_link?: string;
   website_link: string;
   description: string[];
 };
@@ -16,15 +16,30 @@ export const portfolio_data: PortfolioData[] = [
   //   description: [""],
   // },
   {
-    image: "/images/coffee-connoisseur.png",
+    image: "/images/test2.png",
+    // image: "/images/coffee-connoisseur.png",
     title: "Coffee Connoisseur",
-    subtitle: "Next.js + AirTable",
+    subtitle: "Next.js + Typescript + Tailwind CSS + AirTable",
     github_link: "https://github.com/jo-anne-tan/coffee-connoisseur",
     website_link: "https://coffee-connoisseur-nine.vercel.app/",
     description: [
       "This was produced as part of a Next.js Mastery course. The goal is to display several local coffee stores, allow users to view coffee stores near them based on the geolocation api, and view each store's address, name, and upvotes.",
       "Data is stored and updated at Airtable. Each coffee store data is created upon first visit by users. If the coffee store already exists in the database, it will simply retrieve it. Users can also upvote the stores they like.",
       "Through this project, I have learnt about different page rendering methods provided by Next.js and how to implement them, including Static Site Generation, Server Side Rendering, and Incremental Server Regeneration. It is also the first time I've used the browser built-in geolocation api and AirTable.",
+    ],
+  },
+  {
+    image: "/images/sharkbate-webstore.png",
+    title: "Sharkbate Web Store",
+    subtitle: "Next.js + Typescript + Tailwind CSS + Hasura",
+    website_link: "https://m.sharkbate.com.my/store/GSF6E0H5",
+    description: [
+      "This project made for Sharkbate Sdn Bhd is made to supplement the mobile app released on android and ios.",
+      "As such, it was initially meant to only have the e-commerce stores - i.e. the mobile apps' store pages have a share button which links to the corresponding store pages on this web app. Eventually it was decided that users should be able to check on the web app the rewards they have collected, and the purchases they have made.",
+      "User authentication is done with mobile number and an SMS pin which is generated using an internal api.",
+      "User checkout is integrated with BillPlz, a local payment gateway. Users can checkout using Card, FPX, or an e-wallet of their choosing.",
+      "One missing feature in this web app is the ability to view all merchants on this platform. However, it was decided that this is the intended result as it is meant to supplement the mobile apps.",
+      "",
     ],
   },
 ];
